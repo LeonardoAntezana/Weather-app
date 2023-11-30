00c88bbd62991009aaa174fd414cb317aaa44fa7
¨Leonardo <leonardoantezana59@gmail.com>
2023-11-30 12:05:53 -0300

    add description to Location interface

--- src/models/location.model.ts
+++ src/models/location.model.ts
@@ -1,3 +1,10 @@
+/** 
+  @param {string} name - The name of the location;
+  @param {number} lat - The latitude of the location;
+  @param {number} lon - The longitude of the location;
+  @param {string} state - The state of the location;
+**/
+
 export interface Location {
   name: string;
   lat: number;
