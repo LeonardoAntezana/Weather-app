6c7f4a6904b31e097ce16f0c7cdb8e5fe36b87c2
¨Leonardo <leonardoantezana59@gmail.com>
2023-11-30 12:06:31 -0300

    refactoring getLocations service

--- src/services/getLocation.services.ts
+++ src/services/getLocation.services.ts
@@ -5,8 +5,8 @@ import { locationAdapter } from "../adapters";
 /** 
   @param {string} locationName - The name of the location must be string;
 */
-export const getLocations = async (locationName: string): Promise<Location[]> => {
-  let { data } = await axios.get(`${import.meta.env.VITE_API_BASE_URL}geo/1.0/direct?q=${locationName}&limit=8&appid=${import.meta.env.VITE_API_KEY}`);
+export const getLocations = async (locationName: string, limit: number = 8): Promise<Location[]> => {
+  let { data } = await axios.get(`${import.meta.env.VITE_API_BASE_URL}geo/1.0/direct?q=${locationName}&limit=${limit}&appid=${import.meta.env.VITE_API_KEY}`);
   let allLocations: Location[] = data.map((location: any) => locationAdapter(location));
   return allLocations;
 }
